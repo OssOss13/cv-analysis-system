421f89b418133cc3c536c96c96e7f6f600b9621b
Mohannad <mohanados12@gmail.com>
2025-02-24 22:51:09 +0400

    remove serializers

--- documents/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import CV
-
-class CVSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CV
-        fields = ['id', 'file', 'uploaded_at', 'extracted_text']
